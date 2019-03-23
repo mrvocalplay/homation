7137827fd84b488a3d79070eeb95e465505c0cba
Fabian Gasper <info@fgnetworks.de>
2019-03-23 22:19:10 +0100

    e-mail notify, temperature automation

--- automation/temperatures.go
+++ automation/temperatures.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 
 	mon "github.com/mrvocalplay/monitoring"
-	"github.com/mrvocalplay/monitoring/rpi"
 )
 
 // PowerOff Kill Server
 func PowerOff() {
 	fmt.Println("SHUTTING DOWN")
+	// fmt.Println(mon.RunCmd("sudo shutdown -h now"))
 }
 
 // Warn Warn Message
@@ -17,8 +17,11 @@ func Warn(currentTemp string) {
 	fmt.Println("Temperatures Critical: " + currentTemp)
 }
 
-func Temperatures() string {
-	currentTemp := mon.CutStr(rpi.GetCPUTemp(), "temp=", "'C")
-	fmt.Println(currentTemp)
-	return currentTemp
+func Temperatures(currentTemp string) {
+	temp := mon.ToInt(mon.CutStr(currentTemp, "temp=", "'C"))
+	if temp >= 80 {
+		PowerOff()
+	} else if temp >= 50 {
+		Warn(currentTemp)
+	}
 }
--- homation.go
+++ homation.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	a "github.com/mrvocalplay/homation/automation"
-	"github.com/mrvocalplay/homation/log"
+	"github.com/mrvocalplay/monitoring/rpi"
 )
 
 func main() {
-	a.Temperatures()
-	a.Volts()
-	a.Clock()
+	os := strings.TrimRight(rpi.GetOs(), "\r\n")
+	temp := strings.TrimRight(rpi.GetCPUTemp(), "\r\n")
+	volts := strings.TrimRight(rpi.GetCPUVolts(), "\r\n")
+	clock := strings.TrimRight(rpi.GetArmClock(), "\r\n")
+	str := (os + ", " + temp + ", " + volts + ", " + clock)
+	fmt.Println(str)
+	a.Temperatures(temp)
+	// notify.EMail(str)
 
-	log.Log(log.MakeLog(a.Temperatures()))
-	// rpi.GetCPUTemp()
-	// rpi.GetCPUSpannung()
-	// log.Log("tet")
+	// log.Log(log.MakeLog(a.Temperatures()))
 }
